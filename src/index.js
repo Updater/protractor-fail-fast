fd26741e0a7d4e5b0cd12060feb04823d2c94bfa
Patrick Mowrer <pmowrer@gmail.com>
2015-07-15 20:00:23 -0400

    First version.

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import fs from 'fs';
+import {resolve} from 'path';
+import {init, disableSpecs} from 'jasmine-fail-fast';
+
+/**
+ * protractor-fail-fast
+ *
+ * For the lack of a better (known) option, using a file to communicate between test runners.
+ * If the file exists, it means a test has failed in one (or more) of the runners. If a runner
+ * detects that this file exists, it will exit using `jasmine-fail-fast`.
+ *
+ */
+
+const TOUCH_ON_FAIL = resolve(process.cwd(), './.protractor-failed');
+
+export default {
+  // Custom hooks
+
+  /**
+   * Wrapper around `jasmine-fail-fast`'s `init`, which must be called in `onPrepare` as follows:
+   * `jasmine.getEnv().addReporter(failFast.init());`
+   *
+   * Yet to find a way of automating this via a Protractor plugin hook. `jasmine` isn't available
+   * in `setup`.
+   */
+  init: function() {
+    return init();
+  },
+
+  // Protractor hooks
+  setup: function() {
+    // Clean up the "fail file" before starting, in case it exists.
+    // TODO: Would love to clean up on exit, but yet to find a hook/way to ensure that the file
+    // isn't deleted before the last runner exits.
+    unsetFailed();
+  },
+
+  postTest: function(self, passed) {
+    if(!passed) {
+      setFailed();
+    }
+
+    if(hasFailed()) {
+      disableSpecs();
+    }
+  }
+};
+
+function hasFailed() {
+  return fs.existsSync(TOUCH_ON_FAIL);
+}
+
+function setFailed() {
+  fs.closeSync(fs.openSync(TOUCH_ON_FAIL, 'w'));
+}
+
+function unsetFailed() {
+  if(hasFailed()) {
+    fs.unlinkSync(TOUCH_ON_FAIL);
+  }
+}
